c0103e42604186890704ae8d3a834b44e1b74072
Marcin Szachun <marcin.szachun@nokia.com>
2018-01-25 12:38:59 +0100

    Add "do_it" function to repeatably do someting until it succeeds

---
+++ gluten/wait.py
@@ -0,0 +1,12 @@
+import time
+
+def do_it(fun, max_reps=5, delay=0.5):
+    while True:
+        max_reps -= 1
+        try:
+            fun()
+            return
+        except:
+            if max_reps == 0:
+                raise
+            time.sleep(delay)
