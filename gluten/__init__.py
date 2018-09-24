326939007e2bbbc9802bc6e92c3070f724c4c327
Marcin Szachun <marcin.szachun@nokia.com>
2018-09-24 21:40:28 +0200

    Add main __init__.py file that groups imports

---
+++ gluten/__init__.py
@@ -0,0 +1,9 @@
+from gluten.locators import (
+    dynamic_locate, dynamic_locate_many, Locate, LocateMany, LocateGlobal, LocateGlobalMany
+)
+
+from gluten.page import Page
+
+from gluten.webelements.base import WebElement
+
+from gluten.wait import do_it
